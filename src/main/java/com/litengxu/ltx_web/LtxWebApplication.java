08f159ec1b987338b3548930a75976bf8107b6b5
litengxu <645425189@qq.com>
2020-06-10 10:50:26 +0800

    init

---
+++ src/main/java/com/litengxu/ltx_web/LtxWebApplication.java
@@ -0,0 +1,13 @@
+package com.litengxu.ltx_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtxWebApplication.class, args);
+	}
+
+}
